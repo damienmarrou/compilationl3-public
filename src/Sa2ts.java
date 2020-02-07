d73a61cc32b99cb7fb0b676a622d23671055da73
PaulineTeoulle <pa.teoulle@gmail.com>
2020-02-07 13:57:44 +0100

    TP3 Add mass trucs qui marchent

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -1,13 +1,6 @@
-import sa.SaAppel;
-import sa.SaDecFonc;
-import sa.SaDecTab;
-import sa.SaDecVar;
-import sa.SaDepthFirstVisitor;
-import sa.SaNode;
-import sa.SaProg;
-import sa.SaVarIndicee;
-import sa.SaVarSimple;
+import sa.*;
 import ts.Ts;
+import ts.TsItemVar;
 
 public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
@@ -24,52 +17,63 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     }
 
     @Override
-    public Void visit(SaDecVar node){
+    public Void visit(SaDecVar node) {
 
-        if(tableLocale !=null){
-            if(tableLocale.variables.size() < argsLength)
+        if (tableLocale != null) {
+            if(tableLocale.variables.containsKey(node.getNom())) throw new RuntimeException("Variable/Arg loc déjà déclarée");
+            if (tableLocale.variables.size() < argsLength)
                 tableLocale.addParam(node.getNom());
-            else tableLocale.addVar(node.getNom(),1);
-        }
-        else{
-            tableGlobale.addVar(node.getNom(),1);
+            else tableLocale.addVar(node.getNom(), 1);
+        } else {
+            if(tableGlobale.variables.containsKey(node.getNom())) throw new RuntimeException("Variable glob déjà déclarée");
+            tableGlobale.addVar(node.getNom(), 1);
         }
         return super.visit(node);
     }
 
     @Override
-    public Void visit(SaDecTab node){
-
-        tableGlobale.addVar(node.getNom(),node.getTaille());
+    public Void visit(SaDecTab node) {
+        if(tableLocale != null) throw new RuntimeException("Tab non global");
+        tableGlobale.addVar(node.getNom(), node.getTaille());
         return super.visit(node);
     }
+
     @Override
-    public Void visit(SaDecFonc node){
-        if(node.getParametres()== null){
+    public Void visit(SaDecFonc node) {
+        if(tableGlobale.fonctions.containsKey(node.getNom())) throw new RuntimeException("Fonction déjà déclarée");
+        if (node.getParametres() == null) {
             argsLength = 0;
-        }
-        else argsLength = node.getParametres().length();
+        } else argsLength = node.getParametres().length();
         tableLocale = new Ts();
-        tableGlobale.addFct(node.getNom(),argsLength,tableLocale,node);
-        return super.visit(node);
-    }
-    @Override
-    public Void visit(SaVarSimple node){
-
-        return super.visit(node);
-    }
-    @Override
-    public Void visit(SaVarIndicee node){
+        tableGlobale.addFct(node.getNom(), argsLength, tableLocale, node);
         return super.visit(node);
     }
 
     @Override
-    public Void visit(SaAppel node){
+    public Void visit(SaVarSimple node) {
+        TsItemVar var = null;
+        if(tableLocale != null) var = tableLocale.variables.get(node.getNom());
+        if (var == null) var = tableGlobale.variables.get(node.getNom());
+        if(var == null) throw new RuntimeException("Variable glob non définie");
+        if(var.getTaille() > 1) throw new RuntimeException("Entier indicé");
         return super.visit(node);
     }
 
+    @Override //Peut pas faire : Les tableaux ne peuvent jamais être utilisés sans indice
+    public Void visit(SaVarIndicee node) {TsItemVar var = null;
+        if(tableLocale != null) var = tableLocale.variables.get(node.getNom());
+        if (var == null) var = tableGlobale.variables.get(node.getNom());
+        if(var == null) throw new RuntimeException("Variable glob non définie");
 
 
+        return super.visit(node);
+    }
+
+    @Override
+    public Void visit(SaAppel node) {
+        if(!tableGlobale.fonctions.containsKey(node.getNom())) throw new RuntimeException("Fonction non declarée");
+        return super.visit(node);
+    }
 
 
 }
