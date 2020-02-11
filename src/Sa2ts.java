6e3a6522529ca0c115e561e3c3d7fceaeaf240e0
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-02-11 07:58:26 +0100

    do mieux

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -28,12 +28,13 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     public Void visit(SaDecVar node) {
 
         if (tableLocale != null) {
-            if(tableLocale.variables.containsKey(node.getNom())) throw new RuntimeException("Variable/Arg loc déjà déclarée");
+            if (tableLocale.variables.containsKey(node.getNom()))
+                throw new TsException("Variable/Arg loc déjà déclarée");
             if (tableLocale.variables.size() < argsLength)
                 tableLocale.addParam(node.getNom());
             else tableLocale.addVar(node.getNom(), 1);
         } else {
-            if(tableGlobale.variables.containsKey(node.getNom())) throw new RuntimeException("Variable glob déjà déclarée");
+            if (tableGlobale.variables.containsKey(node.getNom())) throw new TsException("Variable glob déjà déclarée");
             tableGlobale.addVar(node.getNom(), 1);
         }
         return super.visit(node);
@@ -41,14 +42,14 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
     @Override
     public Void visit(SaDecTab node) {
-        if(tableLocale != null) throw new RuntimeException("Tab non global");
+        if (tableLocale != null) throw new TsException("Tab non global");
         tableGlobale.addVar(node.getNom(), node.getTaille());
         return super.visit(node);
     }
 
     @Override
     public Void visit(SaDecFonc node) {
-        if(tableGlobale.fonctions.containsKey(node.getNom())) throw new RuntimeException("Fonction déjà déclarée");
+        if (tableGlobale.fonctions.containsKey(node.getNom())) throw new TsException("Fonction déjà déclarée");
         if (node.getParametres() == null) {
             argsLength = 0;
         } else argsLength = node.getParametres().length();
@@ -62,8 +63,8 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         TsItemVar var = null;
         if(tableLocale != null) var = tableLocale.variables.get(node.getNom());
         if (var == null) var = tableGlobale.variables.get(node.getNom());
-        if(var == null) throw new RuntimeException("Variable glob non définie");
-        if(var.getTaille() > 1) throw new RuntimeException("Entier indicé");
+        if (var == null) throw new TsException("Variable glob non définie");
+        if (var.getTaille() > 1) throw new TsException("Entier indicé");
         return super.visit(node);
     }
 
@@ -71,7 +72,7 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     public Void visit(SaVarIndicee node) {TsItemVar var = null;
         if(tableLocale != null) var = tableLocale.variables.get(node.getNom());
         if (var == null) var = tableGlobale.variables.get(node.getNom());
-        if(var == null) throw new RuntimeException("Variable glob non définie");
+        if (var == null) throw new TsException("Variable glob non définie");
 
 
         return super.visit(node);
@@ -82,18 +83,20 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
     @Override
     public Void visit(SaAppel node) {
-        if (!tableGlobale.fonctions.containsKey(node.getNom())) throw new RuntimeException("Fonction non declarée");
-        if (!tableGlobale.fonctions.containsKey("main")) throw new RuntimeException("Il n'existe aucune fonction main");
-        if(tableGlobale.fonctions.containsKey("main") && tableLocale.variables.get("main").getTaille() != 0) throw new RuntimeException("Trop d'argument pour main");
+        // if (!tableGlobale.fonctions.containsKey(node.getNom())) throw new TsException("Fonction non declarée");
+        if (!tableGlobale.fonctions.containsKey(node.getNom())) throw new TsException("Fonction non declarée");
+        if (!tableGlobale.fonctions.containsKey("main")) throw new TsException("Il n'existe aucune fonction main");
+        if (tableGlobale.fonctions.containsKey("main") && tableLocale.variables.get("main").getTaille() != 0)
+            throw new TsException("Trop d'argument pour main");
 
         return super.visit(node);
     }
 
 
-    public class TsException extends Exception {
+    public class TsException extends RuntimeException {
 
-        public TsException(String message, Throwable cause) {
-            super(message, cause);
+        public TsException(String message) {
+            super(message);
         }
     }
 }
--- src/Sa2tsTest.java
+++ src/Sa2tsTest.java
@@ -7,7 +7,6 @@ import sc.parser.ParserException;
 import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.StringReader;
-
 public class Sa2tsTest {
 
     private void buildTs(String code) throws ParserException, IOException, LexerException {
@@ -24,96 +23,96 @@ public class Sa2tsTest {
         buildTs("entier a, entier a; main() {}");
     }
 
-   // @Test(expected = Sa2ts.TsException.class)
-    public void testNoDuplicateArrayGlobalScope() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testNoDuplicateArrayGlobalScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier a[5], entier a[5]; main() {}");
     }
 
    @Test(expected = Sa2ts.TsException.class)
-    public void testNoDuplicateVariableArgumentScope() throws ParserException, IOException, LexerException {
+   public void testNoDuplicateVariableArgumentScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main(entier a, entier a) {}");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testNoDuplicateVariableFunctionScope() throws ParserException, IOException, LexerException {
+    public void testNoDuplicateVariableFunctionScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() entier a, entier a; {}");
     }
 
-    //@Test(expected = Sa2ts.TsException.class)
-    public void testNoFunctionVariableSameNameAsArgument() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testNoFunctionVariableSameNameAsArgument() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main(entier a) entier a; {}");
     }
 
     // TODO : uniquement la portée la plus proche est accessible
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testArraysAreGlobals() throws ParserException, IOException, LexerException {
+    public void testArraysAreGlobals() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() entier a[5]; {}");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testNoFunctionSameNameAsVariableLocalScope() throws ParserException, IOException, LexerException {
+    public void testNoFunctionSameNameAsVariableLocalScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() entier test; {main();} test() {test();}");
     }
 
-    //@Test(expected = Sa2ts.TsException.class)
-    public void testNoFunctionSameNameAsVariableGlobalScope() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testNoFunctionSameNameAsVariableGlobalScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier main; main() {main();}");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testNoFunctionSameNameAsArrayGlobalScope() throws ParserException, IOException, LexerException {
+    public void testNoFunctionSameNameAsArrayGlobalScope() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier main[2]; main() {main();}");
     }
 
-   // @Test(expected = Sa2ts.TsException.class)
-    public void testUsedVariableIsDeclared() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testUsedVariableIsDeclared() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() { a = 1; }");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testUsedArrayIsDeclared() throws ParserException, IOException, LexerException {
+    public void testUsedArrayIsDeclared() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() { a[0] = 1; }");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testArrayNotUsedAsInteger() throws ParserException, IOException, LexerException {
+    public void testArrayNotUsedAsInteger() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier a[2]; main() { a = 0; }");
     }
 
     // TODO
 
-    /*@Test(expected = Sa2ts.TsException.class)
-    public void testIntegerNotUsedAsArray() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testIntegerNotUsedAsArray() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier a; main() { a[0] = 0; }");
-    }*/
+    }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testNoCastsFromArrayToInteger() throws ParserException, IOException, LexerException {
+    public void testNoCastsFromArrayToInteger() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("entier a[2], entier b; main() { b = a; }");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testNoCastsFromIntegerToArray() throws ParserException, IOException, LexerException {
-        /*try {
+    public void testNoCastsFromIntegerToArray() throws ParserException, IOException, LexerException, Sa2ts.TsException {
+        try {
             buildTs("entier a[2], entier b; main() { a = b; }");
         } catch (Sa2ts.TsException e) {
             buildTs("entier a[2]; g() { retour 0; } main() { a = g(); }");
-        }*/
+        }
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testAllFunctionsHaveUniqueNames() throws ParserException, IOException, LexerException {
+    public void testAllFunctionsHaveUniqueNames() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() { main(); } main() { main(); }");
     }
 
-    //@Test(expected = Sa2ts.TsException.class)
-    public void testCalledFunctionIsDeclared() throws ParserException, IOException, LexerException {
+    @Test(expected = Sa2ts.TsException.class)
+    public void testCalledFunctionIsDeclared() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() { g(); }");
     }
 
     @Test(expected = Sa2ts.TsException.class)
-    public void testCalledFunctionIsDeclaredBefore() throws ParserException, IOException, LexerException {
+    public void testCalledFunctionIsDeclaredBefore() throws ParserException, IOException, LexerException, Sa2ts.TsException {
         buildTs("main() { g(); } g() { retour 0; }");
     }
 
