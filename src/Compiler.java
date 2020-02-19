f1d14e85e1cea32e9d136812e4d5973e94d302b1
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-02-19 12:45:22 +0100

    do mieux

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,3 +1,4 @@
+import c3a.C3a;
 import sa.Sa2Xml;
 import sa.SaNode;
 import sc.lexer.Lexer;
@@ -32,8 +33,8 @@ public class Compiler {
 
 			try {
 				//if (0 < args.length) {
-					br = new PushbackReader(new FileReader(fileName), 1024);
-					baseName = removeSuffix(fileName, ".l");
+				br = new PushbackReader(new FileReader(fileName), 1024);
+				baseName = removeSuffix(fileName, ".l");
 				/*} else {
 					System.out.println("il manque un argument");
 				}*/
@@ -51,19 +52,19 @@ public class Compiler {
 				//System.out.println("[SC]");
 				tree.apply(new Sc2Xml(baseName));
 
-                //System.out.println("[SA]");
-                Sc2sa sc2sa = new Sc2sa();
-                tree.apply(sc2sa);
-                SaNode saRoot = sc2sa.getRoot();
-                new Sa2Xml(saRoot, baseName);
+				//System.out.println("[SA]");
+				Sc2sa sc2sa = new Sc2sa();
+				tree.apply(sc2sa);
+				SaNode saRoot = sc2sa.getRoot();
+				new Sa2Xml(saRoot, baseName);
 
 				//System.out.println("[TABLE SYMBOLES]");
-                Ts table = new Sa2ts(saRoot).getTableGlobale();
-                table.afficheTout(baseName);
+				Ts table = new Sa2ts(saRoot).getTableGlobale();
+				table.afficheTout(baseName);
 
-               /*System.out.println("[C3A]");
-                C3a c3a = new Sa2c3a(saRoot, table).getC3a();
-                c3a.affiche(baseName);
+				//System.out.println("[C3A]");
+				C3a c3a = new Sa2c3a(saRoot, table).getC3a();
+				c3a.affiche(baseName);
 
 /*                System.out.println("[NASM]");
                 Nasm nasm = new C3a2nasm(c3a, table).getNasm();
@@ -78,7 +79,7 @@ public class Compiler {
                 fgSolution.affiche(baseName);*/
 			} catch (Exception e) {
 				//System.out.println("fileName = " + fileName);
-				System.out.println("\nFichier n° "+j +"  "+ fileName.substring(fileName.lastIndexOf('\\')+1)+" erreur n° " +i);
+				System.out.println("\nFichier n° " + j + "  " + fileName.substring(fileName.lastIndexOf('\\') + 1) + " erreur n° " + i);
 				i++;
 				//System.out.println(e.getMessage());
 				e.printStackTrace();
@@ -87,36 +88,49 @@ public class Compiler {
 		System.out.println();
 		List<String> fileSA = new ArrayList<>();
 		List<String> fileTS = new ArrayList<>();
+		List<String> fileC3A = new ArrayList<>();
 		File folderSA = new File("test/input");
 		File folderTS = new File("test/input");
 		File[] listOfFilesSA = folderSA.listFiles();
 		File[] listOfFilesTS = folderTS.listFiles();
+		File[] listOfFilesC3A = folderTS.listFiles();
 
 		for (int x = 0; x < listOfFilesSA.length; x++) {
 			if (listOfFilesSA[x].isFile() && listOfFilesSA[x].getName().endsWith(".sa")) {
-				fileSA.add(listOfFilesSA[x].getAbsolutePath().substring(listOfFilesSA[x].getAbsolutePath().lastIndexOf('\\')+1));
+				fileSA.add(listOfFilesSA[x].getAbsolutePath().substring(listOfFilesSA[x].getAbsolutePath().lastIndexOf('\\') + 1));
 			}
 		}
 
 		for (int x = 0; x < listOfFilesTS.length; x++) {
 			if (listOfFilesTS[x].isFile() && listOfFilesTS[x].getName().endsWith(".ts")) {
-				fileTS.add(listOfFilesTS[x].getAbsolutePath().substring(listOfFilesTS[x].getAbsolutePath().lastIndexOf('\\')+1));
+				fileTS.add(listOfFilesTS[x].getAbsolutePath().substring(listOfFilesTS[x].getAbsolutePath().lastIndexOf('\\') + 1));
 			}
 		}
 
 		System.out.println("SA");
-		for(String file : fileSA){
+		for (String file : fileSA) {
 			//System.out.println(file);
 			System.out.print(new String(Runtime.getRuntime().exec("python test/CompareArbre.py " + file + " " + file).getErrorStream().readAllBytes()));
 		}
 		System.out.println("TS");
-		for(String file : fileTS){
+		for (String file : fileTS) {
+			//System.out.println(file);
+			System.out.print(new String(Runtime.getRuntime().exec("python test/CompareArbre.py " + file + " " + file).getErrorStream().readAllBytes()));
+		}
+		for (int x = 0; x < listOfFilesC3A.length; x++) {
+			if (listOfFilesC3A[x].isFile() && listOfFilesC3A[x].getName().endsWith(".c3a")) {
+				fileTS.add(listOfFilesC3A[x].getAbsolutePath().substring(listOfFilesC3A[x].getAbsolutePath().lastIndexOf('\\') + 1));
+			}
+		}
+
+		System.out.println("C3A");
+		for (String file : fileC3A) {
 			//System.out.println(file);
 			System.out.print(new String(Runtime.getRuntime().exec("python test/CompareArbre.py " + file + " " + file).getErrorStream().readAllBytes()));
 		}
 
 
-}
+	}
 
 	public static String removeSuffix(final String s, final String suffix) {
 		if (s != null && suffix != null && s.endsWith(suffix)) {
