f947f4d7da949109a0377533e999d104f0fc188b
PaulineTeoulle <pa.teoulle@gmail.com>
2020-02-28 12:18:23 +0100

    Correctif mais toujours pas d'input

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -1,12 +1,4 @@
-import sa.SaAppel;
-import sa.SaDecFonc;
-import sa.SaDecTab;
-import sa.SaDecVar;
-import sa.SaDepthFirstVisitor;
-import sa.SaNode;
-import sa.SaProg;
-import sa.SaVarIndicee;
-import sa.SaVarSimple;
+import sa.*;
 import ts.Ts;
 import ts.TsItemVar;
 
@@ -14,11 +6,11 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
     Ts tableGlobale = new Ts();
     Ts tableLocale = null;
-    Location location;
+    Ts location = tableGlobale;
     int argsLength = 0;
+    boolean isParam = false;
 
     public Sa2ts(SaNode saRoot) {
-        location = Location.Global;
         visit((SaProg) saRoot);
         checkMainExists();
     }
@@ -31,14 +23,15 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         return tableGlobale;
     }
 
+    private boolean isLocalTable() {
+        return location != tableGlobale;
+    }
+
     @Override
     public Void visit(SaDecVar node) {
-        //Location loc = location;
-        if (tableGlobale.variables.containsKey(node.getNom()))
+        /*if (tableGlobale.variables.containsKey(node.getNom()))
             throw new Sa2ts.TsException("Variable avec le même nom");
-       // tableGlobale.addVar(node.getNom(), 1);
         if (tableLocale != null) {
-            // location = Location.Local;
             if (tableLocale.variables.containsKey(node.getNom()))
                 throw new Sa2ts.TsException("variable locale avec le même nom");
             if (tableLocale.variables.size() < argsLength) {
@@ -49,25 +42,64 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         } else {
             tableGlobale.addVar(node.getNom(), 1);
         }
-        //location = loc;
-        return super.visit(node);
+
+        node.tsItem = isParam
+                ? location.addParam(node.getNom())
+                : location.addVar(node.getNom(), 1);
+        return null;*/
+
+        TsItemVar itemVar = location.getVar(node.getNom());
+        if (itemVar != null)
+            if (isLocalTable())
+                throw new TsException(node.getNom() + " Déjà définie en local");
+            else
+                throw  new TsException(node.getNom() + " Déjà définie en global");
+
+        node.tsItem = isParam
+                ? location.addParam(node.getNom())
+                : location.addVar(node.getNom(), 1);
+
+        return null;
     }
 
     @Override
     public Void visit(SaDecTab node) {
         //location = Location.Global;
-        if (tableLocale != null) throw new TsException("tableau non globale");
+        /*if (isLocalTable()) throw new TsException("tableau non global");
+
         if (tableGlobale.variables.containsKey(node.getNom()))
             throw new TsException("Variable globale tableau déjà déclaré");
-        tableGlobale.addVar(node.getNom(), node.getTaille());
-        return super.visit(node);
+        node.tsItem = location.addVar(node.getNom(), node.getTaille());
+        return null;*/
+
+        if (isLocalTable()) throw new TsException("Pas de déclaration de tableau en lcoal");
+        if (tableGlobale.getVar(node.getNom()) != null) throw new RuntimeException("Dans table global, déja definie");
+
+        node.tsItem = location.addVar(node.getNom(), node.getTaille());
+
+        return null;
     }
 
     @Override
     public Void visit(SaDecFonc node) {
 
         if (tableGlobale.fonctions.containsKey(node.getNom())) throw new TsException("fonction déjà déclaré");
-        if (tableGlobale.variables.containsKey(node.getNom()))
+        node.tsItem = tableGlobale.addFct(
+                node.getNom(),
+                node.getParametres() != null ? node.getParametres().length() : 0,
+                new Ts(),
+                node);
+
+        location = node.tsItem.getTable();
+        isParam = true;
+        if(node.getParametres() != null) node.getParametres().accept(this);
+        isParam = false;
+        if(node.getVariable() != null) node.getVariable().accept(this);
+        node.getCorps().accept(this);
+        location = tableGlobale;
+
+
+       /* if (tableGlobale.variables.containsKey(node.getNom()))
             throw new TsException("Fonction avec le même nom qu'une variable globale");
         if (node.getParametres() == null) {
             argsLength = 0;
@@ -79,14 +111,17 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
             throw new TsException("pas de main sans arguments" + tableGlobale.fonctions.size());
         if (tableGlobale.fonctions.containsKey("main") && !(tableGlobale.fonctions.get("main").getNbArgs() == 0))
             throw new TsException("Arguments dans main pas bon");
-        return super.visit(node);
+        return super.visit(node);*/
+        return null;
     }
 
     @Override
     public Void visit(SaVarSimple node) {
-        TsItemVar var;
+        /*TsItemVar var;
         /*  -- Toute variable utilisée est déclarée en tant que (recherche dans l’ordre)
-        : — (1) Variable locale ou (2) argument de fonction ou (3) variable globale.*/
+        : — (1) Variable locale ou (2) argument de fonction ou (3) variable globale.
+
+
         if (tableGlobale.variables.containsKey(node.getNom())) {
             var = tableGlobale.variables.get(node.nom);
         } else if (tableLocale != null) {
@@ -100,12 +135,19 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
 
         //-- Les entiers ne peuvent jamais être indicés
         if (var.getTaille() > 1) throw new TsException("Entier indicé alors qu'il ne faut pas");
-        return super.visit(node);
+        return null;*/
+
+
+        if(isLocalTable() && location.variables.containsKey(node.getNom())) node.tsItem = location.variables.get(node.getNom());
+        else node.tsItem = tableGlobale.getVar(node.getNom());
+        if(node.tsItem == null) throw new TsException("Pas définie");
+        if(node.tsItem.getTaille() > 1) throw  new TsException("Entier indicé alors qu'il ne faut pas");
+        return null;
     }
 
     @Override
     public Void visit(SaVarIndicee node) {
-        TsItemVar var = null;
+       /* TsItemVar var = null;
         if (tableLocale != null) {
             var = tableLocale.variables.get(node.getNom());
         }
@@ -115,21 +157,31 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         if (var == null) throw new TsException("Variable glob non définie");
 
         if (var.getTaille() < 1) throw new TsException("tableau sans indice");
-        return super.visit(node);
+        return super.visit(node);*/
+
+       if(isLocalTable() && location.variables.containsKey(node.getNom())) node.tsItem = location.variables.get(node.getNom());
+       else node.tsItem = tableGlobale.getVar(node.getNom());
+       if(node.tsItem == null) throw  new TsException("Pas définie");
+       if(node.tsItem.getTaille() < 1) throw  new TsException("Tableau sans indice");
+
+        return null;
     }
 
     @Override
     public Void visit(SaAppel node) {
         if (!tableGlobale.fonctions.containsKey(node.getNom())) throw new TsException("Fonction non declarée");
-        int length = 0;
+        else node.tsItem = tableGlobale.getFct(node.getNom());
+        int length;
         if (node.getArguments() != null) {
             length = node.getArguments().length();
         }
-        if (length != tableGlobale.fonctions.get(node.getNom()).getNbArgs()) {
+        else length = 0;
+        if (argsLength > 0) node.getArguments().accept(this);
+        if (length != node.tsItem.nbArgs) {
 
             throw new TsException("Pas le bon nombre d'argument pour la fonction");
         }
-        return super.visit(node);
+        return null;
     }
 
     static class TsException extends RuntimeException {
