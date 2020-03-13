abf40e60df8dce7a7fcbe6a64553385b8242f319
damienmarrou <44019360+damienmarrou@users.noreply.github.com>
2020-03-13 13:01:38 +0100

    do mieux

---
+++ src/c3a/C3a2nasm.java
@@ -0,0 +1,13 @@
+package c3a;
+
+import nasm.Nasm;
+import ts.Ts;
+
+public class C3a2nasm {
+    public C3a2nasm(C3a c3a, Ts table) {
+    }
+
+    public Nasm getNasm() {
+        return null;
+    }
+}
