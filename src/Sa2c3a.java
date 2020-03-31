8c034be6278284665124cc0eec11ca8577f0e0d0
PaulineTeoulle <pa.teoulle@gmail.com>
2020-03-31 16:27:44 +0200

    Revert "Sa2c3a commenté + refactor"
    This reverts commit c2c15c1b

--- src/Sa2c3a.java
+++ src/Sa2c3a.java
@@ -28,13 +28,10 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         super.defaultOut(node);
     }
 
-    /**
-     * Permet de visiter tout le code
-     * @param node le noeud à visiter
-     */
     public C3aOperand visit(SaProg node) {
         return super.visit(node);
     }
+
 
     @Override
     public C3aOperand visit(SaDecVar node) {
@@ -50,10 +47,7 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour la délaration de fonction
-     * @param node le noeud à visiter
-     */
+
     public C3aOperand visit(SaDecFonc node) {
         c3a.ajouteInst(new C3aInstFBegin(node.tsItem, "entree fonction"));
         if (node.getParametres() != null)
@@ -68,60 +62,37 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'expression d'entier
-     * @param node le noeud à visiter
-     */
+
     @Override
-    public C3aOperand visit(SaExpInt node) {
+    public C3aOperand visit(SaExpInt node) { // OK
         return new C3aConstant(node.getVal());
     }
 
-    /**
-     * Créer le code 3 adresses pour l'expression d'une variable
-     * @param node le noeud à visiter
-     */
     @Override
-    public C3aOperand visit(SaExpVar node) {
+    public C3aOperand visit(SaExpVar node) { // OK
         return node.getVar().accept(this);
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction d'écriture
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaInstEcriture node) {
-        C3aOperand operand = node.getArg().accept(this);
-        c3a.ajouteInst(new C3aInstWrite(operand, ""));
+        C3aOperand op = node.getArg().accept(this);
+        c3a.ajouteInst(new C3aInstWrite(op, ""));
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction de retour
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaInstRetour node) {
         c3a.ajouteInst(new C3aInstReturn(node.getVal().accept(this), ""));
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction de lecture
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpLire node) {
-        C3aTemp temp = c3a.newTemp();
+        var temp = c3a.newTemp();
         c3a.ajouteInst(new C3aInstRead(temp, ""));
         return temp;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction tant que
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaInstTantQue node) {
 
@@ -137,41 +108,26 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction d'affectation
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaInstAffect node) {
-        C3aOperand operand1 = node.getRhs().accept(this);
-        C3aOperand operand2 = node.getLhs().accept(this);
+        C3aOperand op1 = node.getRhs().accept(this);
+        C3aOperand op2 = node.getLhs().accept(this);
 
-        c3a.ajouteInst(new C3aInstAffect(operand1, operand2, ""));
+        c3a.ajouteInst(new C3aInstAffect(op1, op2, ""));
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour la déclaration de variable simple
-     * @param node le noeud à visiter
-     */
     @Override
-    public C3aOperand visit(SaVarSimple node) {
+    public C3aOperand visit(SaVarSimple node) {//OK
         return new C3aVar(node.tsItem, null);
     }
 
-    /**
-     * Créer le code 3 adresses pour la déclaration de variable indicée
-     * @param node le noeud à visiter
-     */
     @Override
-    public C3aOperand visit(SaVarIndicee node) {
+    public C3aOperand visit(SaVarIndicee node) { //OK
         return new C3aVar(node.tsItem, node.getIndice().accept(this));
     }
 
-    /**
-     * Créer le code 3 adresses pour l'instruction d'appel
-     * @param node le noeud à visiter
-     */
+
     @Override
     public C3aOperand visit(SaAppel node) {
         C3aFunction c3aFunction = new C3aFunction(node.tsItem);
@@ -180,10 +136,6 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return c3aFunction;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'appel d'expression
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpAppel node) {
         C3aTemp temp = c3a.newTemp();
@@ -196,154 +148,120 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return temp;
     }
 
-    /**
-     * Créer le code 3 adresses pour l'addition d'expression
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpAdd node) {
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
-        C3aTemp temp = c3a.newTemp();
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
+        var tempRes = c3a.newTemp();
 
-        c3a.ajouteInst(new C3aInstAdd(operand1, operand2, temp, ""));
-        return temp;
+        c3a.ajouteInst(new C3aInstAdd(op1, op2, tempRes, ""));
+        return tempRes;
     }
 
-    /**
-     * Créer le code 3 adresses pour la soustraction d'expression
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpSub node) {
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        C3aTemp temp = c3a.newTemp();
-        c3a.ajouteInst(new C3aInstSub(operand1, operand2, temp, ""));
-        return temp;
+        var tempRes = c3a.newTemp();
+        c3a.ajouteInst(new C3aInstSub(op1, op2, tempRes, ""));
+        return tempRes;
     }
 
-    /**
-     * Créer le code 3 adresses pour la multiplication d'expression
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpMult node) {
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        C3aTemp temp = c3a.newTemp();
-        c3a.ajouteInst(new C3aInstMult(operand1, operand2, temp, ""));
+        var tempRes = c3a.newTemp();
+        c3a.ajouteInst(new C3aInstMult(op1, op2, tempRes, ""));
 
-        return temp;
+        return tempRes;
     }
 
-    /**
-     * Créer le code 3 adresses pour la division d'expression
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpDiv node) {
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        C3aTemp temp = c3a.newTemp();
-        c3a.ajouteInst(new C3aInstDiv(operand1, operand2, temp, ""));
+        var tempRes = c3a.newTemp();
+        c3a.ajouteInst(new C3aInstDiv(op1, op2, tempRes, ""));
 
-        return temp;
+        return tempRes;
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition d'infériorité
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpInf node) {
         C3aLabel label = c3a.newAutoLabel();
-        C3aTemp temp = c3a.newTemp();
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aTemp tempTest = c3a.newTemp();
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        c3a.ajouteInst(new C3aInstAffect(c3a.True, temp, ""));
-        c3a.ajouteInst(new C3aInstJumpIfLess(operand1, operand2, label, ""));
-        c3a.ajouteInst(new C3aInstAffect(c3a.False, temp, ""));
+        c3a.ajouteInst(new C3aInstAffect(c3a.True, tempTest, ""));
+        c3a.ajouteInst(new C3aInstJumpIfLess(op1, op2, label, ""));
+        c3a.ajouteInst(new C3aInstAffect(c3a.False, tempTest, ""));
         c3a.addLabelToNextInst(label);
 
-        return temp;
+        return tempTest;
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition d'égalité
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpEqual node) {
         C3aLabel label = c3a.newAutoLabel();
 
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
 
-        C3aTemp temp = c3a.newTemp();
-        c3a.ajouteInst(new C3aInstAffect(c3a.True, temp, ""));
-        c3a.ajouteInst(new C3aInstJumpIfEqual(operand1, operand2, label, ""));
-        c3a.ajouteInst(new C3aInstAffect(c3a.False, temp, ""));
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
+
+        C3aTemp tempTest = c3a.newTemp();
+        c3a.ajouteInst(new C3aInstAffect(c3a.True, tempTest, ""));
+        c3a.ajouteInst(new C3aInstJumpIfEqual(op1, op2, label, ""));
+        c3a.ajouteInst(new C3aInstAffect(c3a.False, tempTest, ""));
         c3a.addLabelToNextInst(label);
-        return temp;
+        return tempTest;
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition "et"
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpAnd node) {
-        C3aLabel label1 = c3a.newAutoLabel();
-        C3aLabel label2 = c3a.newAutoLabel();
-        C3aTemp temp = c3a.newTemp();
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aLabel e1 = c3a.newAutoLabel();
+        C3aLabel e2 = c3a.newAutoLabel();
+        C3aTemp tempTest = c3a.newTemp();
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        c3a.ajouteInst(new C3aInstJumpIfEqual(operand1, c3a.False, label2, ""));
-        c3a.ajouteInst(new C3aInstJumpIfEqual(operand2, c3a.False, label2, ""));
-        c3a.ajouteInst(new C3aInstAffect(c3a.True, temp, ""));
-        c3a.ajouteInst(new C3aInstJump(label1, ""));
-        c3a.addLabelToNextInst(label2);
-        c3a.ajouteInst(new C3aInstAffect(c3a.False, temp, ""));
-        c3a.addLabelToNextInst(label1);
+        c3a.ajouteInst(new C3aInstJumpIfEqual(op1, c3a.False, e2, ""));
+        c3a.ajouteInst(new C3aInstJumpIfEqual(op2, c3a.False, e2, ""));
+        c3a.ajouteInst(new C3aInstAffect(c3a.True, tempTest, ""));
+        c3a.ajouteInst(new C3aInstJump(e1, ""));
+        c3a.addLabelToNextInst(e2);
+        c3a.ajouteInst(new C3aInstAffect(c3a.False, tempTest, ""));
+        c3a.addLabelToNextInst(e1);
 
-        return temp;
+        return tempTest;
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition "ou"
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpOr node) {
-        C3aLabel label1 = c3a.newAutoLabel();
-        C3aLabel label2 = c3a.newAutoLabel();
-        C3aTemp temp = c3a.newTemp();
+        C3aLabel e1 = c3a.newAutoLabel();
+        C3aLabel e2 = c3a.newAutoLabel();
+        C3aTemp tempTest = c3a.newTemp();
 
-        C3aOperand operand1 = node.getOp1().accept(this);
-        C3aOperand operand2 = node.getOp2().accept(this);
+        C3aOperand op1 = node.getOp1().accept(this);
+        C3aOperand op2 = node.getOp2().accept(this);
 
-        c3a.ajouteInst(new C3aInstJumpIfNotEqual(operand1, c3a.False, label2, ""));
-        c3a.ajouteInst(new C3aInstJumpIfNotEqual(operand2, c3a.False, label2, ""));
-        c3a.ajouteInst(new C3aInstAffect(c3a.False, temp, ""));
 
-        c3a.ajouteInst(new C3aInstJump(operand1, ""));
-        c3a.addLabelToNextInst(label2);
-        c3a.ajouteInst(new C3aInstAffect(c3a.True, temp, ""));
-        c3a.addLabelToNextInst(label1);
+        c3a.ajouteInst(new C3aInstJumpIfNotEqual(op1, c3a.False, e2, ""));
+        c3a.ajouteInst(new C3aInstJumpIfNotEqual(op2, c3a.False, e2, ""));
+        c3a.ajouteInst(new C3aInstAffect(c3a.False, tempTest, ""));
 
-        return temp;
+        c3a.ajouteInst(new C3aInstJump(e1, ""));
+        c3a.addLabelToNextInst(e2);
+        c3a.ajouteInst(new C3aInstAffect(c3a.True, tempTest, ""));
+        c3a.addLabelToNextInst(e1);
+
+        return tempTest;
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition "non"
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaExpNot node) {
         C3aTemp temp = c3a.newTemp();
@@ -358,10 +276,7 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
 
     }
 
-    /**
-     * Créer le code 3 adresses pour la condition "si"
-     * @param node le noeud à visiter
-     */
+
     @Override
     public C3aOperand visit(SaInstSi node) {
 
@@ -387,10 +302,6 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         return null;
     }
 
-    /**
-     * Créer le code 3 adresses pour les listes d'expression et continue le parcours
-     * @param node le noeud à visiter
-     */
     @Override
     public C3aOperand visit(SaLExp node) {
         c3a.ajouteInst(new C3aInstParam(node.getTete().accept(this), ""));
@@ -399,28 +310,17 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
         else return null;
     }
 
-    /**
-     * Permet de visiter les noeuds de la liste d'instruction
-     * @param node le noeud à visiteretourr
-     */
+
     @Override
     public C3aOperand visit(SaLInst node) {
         return super.visit(node);
     }
 
-    /**
-     * Permet de visiter les noeuds de la liste de déclaration
-     * @param node le noeud à visiteretourr
-     */
     @Override
     public C3aOperand visit(SaLDec node) {
         return super.visit(node);
     }
 
-    /**
-     * Permet de visiter les noeuds des instructions du bloc
-     * @param node le noeud à visiteretourr
-     */
     @Override
     public C3aOperand visit(SaInstBloc node) {
         return super.visit(node);
